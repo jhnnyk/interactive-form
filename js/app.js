d710cb345aa82f77f11c182ba4e6ed578e052158
John Knotts <jhnnyk@gmail.com>
2016-12-24 13:53:15 -0700

    complete project steps 1 and 2 for the basic info part of the form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log('here!');
+var nameField = document.getElementById('name');
+var otherJobRole = document.getElementById('other-title');
+var selectJobRole = document.getElementById('title');
+
+//  set focus on first text field when the page loads
+nameField.focus();
+
+//  hide "Other Job Role" text field unless "Other"
+//  is selected from the "Job Role" drop down menu
+otherJobRole.style.display = "none";
+
+var showOtherJobRoleField = function() {
+  if (selectJobRole.value === 'other') {
+    otherJobRole.style.display = "initial";
+  } else {
+    otherJobRole.style.display = "none";
+  }
+}
+
+selectJobRole.addEventListener("change", showOtherJobRoleField)
